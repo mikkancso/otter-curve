3436fc59a97fc94e448d6199edee739ae21085b8
Miklós Tolnai <mikkancso@gmail.com>
2025-10-26 13:32:28 +0100

    add environment variable management

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,43 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true,
-  },
-  mode: 'development',
-  devtool: 'source-map',
-  devServer: {
-    port: 8080,
-    hot: true,
-    open: true,
-    static: {
-      directory: path.join(__dirname, 'public'),
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
+    mode: 'development',
+    devtool: 'source-map',
+    devServer: {
+        port: 8080,
+        hot: true,
+        open: true,
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+        new Dotenv(),
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
 };
